8b18ea0658cf4cfc68c748fcac150fbffd7babed
patthanapong <neversay.987@hotmail.com>
2017-06-23 17:29:45 +0700

    add label form create

--- resources/views/deposit/create.blade.php
+++ resources/views/deposit/create.blade.php
@@ -15,22 +15,32 @@
 
 <form action="/dolladeposit" method="POST">
     {{ csrf_field() }}
+    
+    <label for="username">username</label>
     <input type="text" name="username" class="form-control" placeholder="username"><br>
-
+    
+    <label for="balance">balance</label>
     <input type="text" name="balance" class="form-control" placeholder="balance"><br>
 
+    <label for="bankdeposit">bankdeposit</label>   
     <input type="text" name="bankdeposit" class="form-control" placeholder="bankdeposit"><br>
 
+    <label for="accountnumberdeposit">accountnumberdeposit</label>
     <input type="text" name="accountnumberdeposit" class="form-control" placeholder="accountnumberdeposit"><br>
 
+    <label for="accontnamedeposit">accontnamedeposit</label>
     <input type="text" name="accontnamedeposit" class="form-control" placeholder="accontnamedeposit"><br>
 
+    <label for="datetime">datetime</label>
     <input type="text" class="form-control"  name="datetime" id="dateInput" placeholder="datetime"><br>
 
+    <label for="channeldeposit">channeldeposit</label>
     <input type="text" name="channeldeposit" class="form-control" placeholder="channeldeposit"><br>
 
+    <label for="tel">tel</label>
     <input type="text" name="tel" class="form-control" placeholder="tel"><br>
 
+    <label for="opinion">opinion</label>
     <input type="text" name="opinion" class="form-control" placeholder="opinion"><br>
 
     <input type="submit"  class="btn btn-primary" value="Save">
